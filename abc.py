a6f1fdf950cc9c28c6930a0b4c36ff1a0a77ec59
MrEricL <eli6@stuy.edu>
2017-09-15 14:04:35 -0400

    Add files via upload

--- abc.py
+++ abc.py
@@ -1,20 +1,42 @@
 import csv 
 import sys
+import random
 jobs=[]
 chance=[]
-d={}
+firstline= False
+percent=0;
+val=0 #to make the precent
+
 
 f = open('occupations.csv', 'r')
 reader = csv.reader(f)
 
 for row in reader:
-    jobs.append(row[0])
-    chance.append(row[1])
-f.close()
-jobs=jobs[1:-1]
-chance=chance[1:-1]
+    if firstline:
+        jobs.append(row[0])
+        val+=float(row[1])
+        chance.append(val)
+    else:
+        firstline=True
 
-for x in range (len(chance)):
+
+
+f.close()
+jobs=jobs[:-1]
+chance=chance[:-1]
+
+d= dict(zip(jobs, chance))
+#print d
+
+percent = random.uniform(0, 99.8)
+print percent
+for key in d:
+    if (d[key] >= percent):
+        print key
+        break
+        
+    else:
+        print key + " failed"
+        print 
     
-d = dict(zip(jobs, chance))
-print d
+
